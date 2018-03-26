d6382a8520027d0a722571b812cf7100c5d3b068
bezmoshyk@yandex.ru <Alex_20th>
2018-03-27 00:57:25 +0300

    change view

--- frontend/scripts/view.js
+++ frontend/scripts/view.js
@@ -1,37 +1,48 @@
 let req = new XMLHttpRequest();
 req.open('GET', 'http://localhost:8080/json/file?fileName=file');
+
 req.onload = function () {
+
+    let form = document.createElement('form');
+    form.onscroll = true;
+
     let ourData = req.responseText;
-    readJSON(JSON.parse(ourData));
+    readJSON(form, JSON.parse(ourData));
+
+    let div = document.createElement('div');
+    div.insertAdjacentElement('beforeend', form);
+    document.getElementById('content').insertAdjacentElement('afterend', div);
 };
 
 req.send();
 
-function readJSON(data) {
-    for (let i in data) {
-        document.body.appendChild(document.createElement('br'));
-        console.log(typeof data[i]);
-        let value = document.createElement('p');
-        value.innerHTML = i + ' ' + ((typeof(data[i]) !== 'object')
-            ? '<input type="text" value=' + data[i] + ' disabled/>' : '') + '<br/>';
-        if (typeof data[i] === 'object'){
-            document.body.appendChild(value);
-            readJSON(data[i]);
+function readJSON(form, data) {
+
+    for (let head in data) {
+
+        let h4 = document.createElement('h4');
+        h4.innerHTML = head + ': ';
+
+        form.insertAdjacentElement('beforeend', h4);
+
+        if (typeof data[head] === 'object') {
+            readJSON(form, data[head]);
         } else {
-            let text = document.createElement('text');
-            text.innerHTML = i + ': ';
-            document.body.appendChild(text);
-            console.log('type of ' + typeof data[i]);
-            let input = document.createElement('input');
-            input.type = 'text';
-            input.value = data[i];
-            input.disabled = true;
-            input.afterprint = '<br/>';
 
-            console.log('value ' + value);
-            document.body.appendChild(input);
-            document.body.appendChild(document.createElement('br'));
+            let ifValue;
+            if (typeof data[head] !== 'object') {
+                ifValue = document.createElement('input');
+                ifValue.classList.add('input-text');
+                ifValue.type = 'text';
+                ifValue.value = data[head];
+                ifValue.disabled = true;
+            } else {
+                ifValue = '';
+            }
 
+            if (ifValue) {
+                form.insertAdjacentElement('beforeend', ifValue);
+            }
         }
     }
 }
